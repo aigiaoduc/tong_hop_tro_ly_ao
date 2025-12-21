32580f40080cee4bf0b976eda248f686ff5182ad
aigiaoduc <tranhongquan6995@gmail.com>
2025-12-21 14:43:34 +0700

    update

--- apps/tro_ly_veo3_sora/script.js
+++
@@ -1,5 +0,0 @@
-// We use a flag to ensure this only runs once per session
-if (!sessionStorage.getItem('veo3sora_redirected')) {
-    window.open('https://gemini.google.com/gem/1-FTkAWaFHNUEUKbys6eGhSgFpFP8TxvJ?usp=sharing', '_blank');
-    sessionStorage.setItem('veo3sora_redirected', 'true');
-}
